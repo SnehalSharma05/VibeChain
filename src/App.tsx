4efc72b3a5d24e7b9eaf102e8c3ee4a2ca924e50
SweetBunny123 <your_new_email@example.com>
2024-11-10 11:11:45 +0530

    Main Move.file

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,13 @@
-// import React from "react";
-// import "./App.css";
-// import MainBody from "./components/UserDashboard";
-
-// function App() {
-//   return (
-//     <div className="relative">
-//       <div className="absolute inset-0">
-//         <MainBody />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-import { MusicMarketplaceExamples } from './components/MarketPlaceSDK';
+import React from "react";
+import "./App.css";
+import MainBody from "./components/UserDashboard";
 
 function App() {
-  const runTest = () => {
-    MusicMarketplaceExamples.runExamples()
-      .then(() => console.log("Examples finished successfully"))
-      .catch(error => console.error("Error running examples:", error));
-  }
-
   return (
-    <div>
-      <h1>Music Marketplace SDK Test</h1>
-      <button onClick={runTest}>Run Examples</button>
-      <div>Check console for results</div>
+    <div className="relative">
+      <div className="absolute inset-0">
+        <MainBody />
+      </div>
     </div>
   );
 }
